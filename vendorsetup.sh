1729f4d2de8e797da8a995df109788aae4dd78b6
Sushrut1101 <guptasushrut@gmail.com>
2021-07-23 14:40:14 +0530

    juice: Update the Build Vars

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,22 +1,23 @@
 #
-# Copyright (C) 2020 The Android Open Source Project
-# Copyright (C) 2020 The TWRP Open Source Project
-# Copyright (C) 2020 SebaUbuntu's TWRP device tree generator
+#	This file is part of the OrangeFox Recovery Project
+# 	Copyright (C) 2020-2021 The OrangeFox Recovery Project
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+#	OrangeFox is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	any later version.
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+#	OrangeFox is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
 #
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# 	This software is released under GPL version 3 or any later version.
+#	See <http://www.gnu.org/licenses/>.
 #
-
-FDEVICE="citrus"
+# 	Please maintain this if you use this script or any part of it
+#
+FDEVICE="juice"
 
 fox_get_target_device() {
 local chkdev=$(echo "$BASH_SOURCE" | grep $FDEVICE)
@@ -35,56 +36,67 @@ fi
 if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
    	export TW_DEFAULT_LANGUAGE="en"
 	export LC_ALL="C"
-	export ALLOW_MISSING_DEPENDENCIES=true
+ 	export ALLOW_MISSING_DEPENDENCIES=true
+	export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
+	export TARGET_DEVICE_ALT="citrus,lime"
 	export FOX_RECOVERY_SYSTEM_PARTITION="/dev/block/mapper/system"
 	export FOX_RECOVERY_VENDOR_PARTITION="/dev/block/mapper/vendor"
-    	export OF_QUICK_BACKUP_LIST="/boot;/data;"
+    export OF_QUICK_BACKUP_LIST="/boot;/data;"
+    export FOX_BUGGED_AOSP_ARB_WORKAROUND="1588606644"
+
+	# Screen Settings
+	export OF_SCREEN_H=2340
+	export OF_STATUS_H=80
+	export OF_STATUS_INDENT_LEFT="48"
+	export OF_STATUS_INDENT_RIGHT="48"
+	export OF_ALLOW_DISABLE_NAVBAR="0"
+
 	export OF_USE_MAGISKBOOT=1
 	export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
-	export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
-	export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
+	export FOX_USE_SPECIFIC_MAGISK_ZIP=~/Magisk/Magisk-v21.4.zip
 	export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
+	export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
 	export OF_NO_MIUI_PATCH_WARNING=1
+	
+	# OTA
+    export OF_DISABLE_MIUI_OTA_BY_DEFAULT=0
+    export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=1
+    export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
+    export OF_OTA_BACKUP_STOCK_BOOT_IMAGE=1
+    
+    # R11.1 Setings
+    export FOX_BUILD_TYPE="Stable-Unofficial"
+    export OF_MAINTAINER="Sushrut Gupta"
+    export OF_MAINTAINER_AVATAR="misc/Sushrut1101.png"
+    export FOX_VERSION="R11.1"
+    
+	export FOX_ENABLE_APP_MANAGER=1
 	export FOX_USE_BASH_SHELL=1
 	export FOX_ASH_IS_BASH=1
 	export FOX_USE_NANO_EDITOR=1
 	export FOX_USE_TAR_BINARY=1
+	export FOX_USE_SED_BINARY=1
+	export FOX_USE_XZ_UTILS=1
 	export OF_SKIP_MULTIUSER_FOLDERS_BACKUP=1
-	export OF_CLOCK_POS=1
-	export FOX_DELETE_AROMAFM=1
-
-	# OTA
-	export OF_KEEP_DM_VERITY_FORCED_ENCRYPTION=1
-	export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=1
-        export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
-	export OF_DISABLE_MIUI_OTA_BY_DEFAULT=1
-
-	# screen settings
-        export OF_SCREEN_H=2340
-        export OF_STATUS_H=80
-        export OF_STATUS_INDENT_LEFT=48
-        export OF_STATUS_INDENT_RIGHT=48
-	export OF_HIDE_NOTCH=1
-
-        # R11
-    	export FOX_R11=1
+    export FOX_R11=1
+    export OF_FLASHLIGHT_ENABLE=1
+    export FOX_DELETE_AROMAFM=1
 	export OF_PATCH_AVB20=1
-    	export FOX_ADVANCED_SECURITY=1
-    	export OF_USE_TWRP_SAR_DETECT=1
+	export OF_FBE_METADATA_MOUNT_IGNORE="1"
+	export OF_HIDE_NOTCH="1"
+	export FOX_REPLACE_BUSYBOX_PS="0"
+	export FOX_SKIP_ZIP_BINARY="1"
+	export FOX_ADVANCED_SECURITY=1
 
-	# Fix dtb.img error
-	mkdir -p out/target/product/juice/testdir/
-	rmdir out/target/product/juice/testdir/
-	cp -r device/xiaomi/juice/prebuilt/dtb.img out/target/product/juice
+	# Run a Process After Formatting Data to Work-Around MTP Issues
+	export OF_RUN_POST_FORMAT_PROCESS=1
 
+	# let's see what are our build VARs
 	if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
   	   export | grep "FOX" >> $FOX_BUILD_LOG_FILE
   	   export | grep "OF_" >> $FOX_BUILD_LOG_FILE
    	   export | grep "TARGET_" >> $FOX_BUILD_LOG_FILE
   	   export | grep "TW_" >> $FOX_BUILD_LOG_FILE
  	fi
-
 fi
 #
-
-
